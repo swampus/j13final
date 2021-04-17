6e33e44776af853cf693ecad01fcef1254510c35
swampus <dmitrijs.gavrilovs.swampus@gmail.com>
2021-04-17 14:08:34 +0300

    missing SQL file

---
+++ src/main/resources/db/migration/V1_0_5__update_book.sql
@@ -0,0 +1,6 @@
+UPDATE BOOK set user_id = 1 WHERE id = 1;
+UPDATE BOOK set user_id = 1 WHERE id = 2;
+UPDATE BOOK set user_id = 2 WHERE id = 3;
+UPDATE BOOK set user_id = 2 WHERE id = 4;
+UPDATE BOOK set user_id = 2 WHERE id = 5;
+UPDATE BOOK set user_id = 1 WHERE id = 6;
