8343ddb2e840dc443600ef4337d0c5694b19ea04
swampus <dmitrijs.gavrilovs.swampus@gmail.com>
2021-04-17 14:24:43 +0300

    missing SQL file

--- src/main/java/com/company/handler/GlobalExceptionHandler.java
+++ src/main/java/com/company/handler/GlobalExceptionHandler.java
@@ -3,33 +3,50 @@ package com.company.handler;
 import com.company.dto.ErrorDTO;
 import com.company.exception.EmailAlreadyExistsException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
 
 
 @ControllerAdvice
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+@RestController
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(value
             = {EmailAlreadyExistsException.class})
     public ResponseEntity<ErrorDTO> handleEmailAlreadyExistsException(
             EmailAlreadyExistsException ex, WebRequest request) {
-        ErrorDTO errorDTO = new ErrorDTO(ex.getMessage(), request.getContextPath(), new Date());
+        ErrorDTO errorDTO = new ErrorDTO(ex.getMessage(), ((ServletWebRequest) request).getRequest().getRequestURI()
+                , new Date());
         return ResponseEntity.ok(errorDTO);
     }
 
+    //Handle rest exception
     @ExceptionHandler(value
             = {Exception.class})
-    public ResponseEntity<ErrorDTO> handleValidationException(
-            Exception ex,
-            WebRequest request) {
+    public ResponseEntity<ErrorDTO> handleValidationException(Exception ex, WebRequest request) {
+        ErrorDTO errorDTO = new ErrorDTO(ex.getMessage(),
+                ((ServletWebRequest) request).getRequest().getRequestURI(), new Date());
+        return ResponseEntity.ok(errorDTO);
+    }
 
-        ErrorDTO errorDTO = new ErrorDTO(ex.getMessage(), request.getContextPath(), new Date());
+    //Handle validation
+    @ExceptionHandler(value
+            = {MethodArgumentNotValidException.class})
+    public ResponseEntity<ErrorDTO> handleValidationException(MethodArgumentNotValidException ex, WebRequest request) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for(ObjectError ob : ex.getAllErrors() ){
+            stringBuilder.append(ob.getObjectName()).append(" ").append(ob.getDefaultMessage()).append(" ");
+        }
+
+        ErrorDTO errorDTO = new ErrorDTO(stringBuilder.toString(),
+                ((ServletWebRequest) request).getRequest().getRequestURI(), new Date());
         return ResponseEntity.ok(errorDTO);
     }
 
