ad7420c40e0d7164cf8fbe6da7a5eb25d3d77e3e
swampus <dmitrijs.gavrilovs.swampus@gmail.com>
2021-05-01 13:48:20 +0300

    google OAUTH

---
+++ src/main/java/com/company/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.company.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Profile({"oauth"})
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2Login();
+    }
+}
--- src/main/java/com/company/config/WebConfig.java
+++ src/main/java/com/company/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@Profile("dev")
+@Profile({"dev", "oauth"})
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
